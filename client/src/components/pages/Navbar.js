5dde3703b9acbd028a4f98153a12e74451497898
githelsui <githelsuico@gmail.com>
2022-01-18 20:30:37 -0800

    set up navbar

---
+++ client/src/components/pages/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar() {
+  return <>
+      <div className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo">
+                Viinyl
+            </Link>
+          </div>
+      </div>
+  </>
+}
+
+export default Navbar;
