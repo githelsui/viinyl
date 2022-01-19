d006badddf212145b2d5c5c18c40ce7d3fd32475
githelsui <githelsuico@gmail.com>
2022-01-19 09:44:52 -0800

    fix searchbar ui

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Searchbar.css';
+import { Link } from 'react-router-dom'
 import { Button } from './Button'
 
 function Searchbar() {
   return (
       <div className='searchbar'>
-        <div className='searchbar-dropdown'>
+        <div className='dropdown'>
             <input type='text' class='form-control' placeholder='search...'/>
         </div>
       </div>
