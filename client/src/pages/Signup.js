1ea6f4cf6fb9c64bf3d377b818931e5830ec152a
githelsui <githelsuico@gmail.com>
2022-01-19 20:02:20 -0800

    sign up page ui

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Signup.css'
+import TypeAnimation from 'react-type-animation'
 import { GoogleLogin, GoogleLogout } from 'react-google-login'
 
 const handleLogin = (response) => {
@@ -9,8 +10,18 @@ const handleLogin = (response) => {
 
 function Signup() {
   return (
-    <div>
-        <GoogleLogin
+    <div className='log-wrapper'>
+        <TypeAnimation
+            cursor={false}
+            sequence={['Viinyl:']}
+            wrapper='h2'
+        />
+        <div> the platform for record collectors and enthusiasts </div> 
+        <div className='signup-info'>/share your collection</div> 
+        <div className='signup-info'>/explore new records</div> 
+        <div className='signup-info'>/add to your wishlist</div> 
+        <div className='signup-info'>/connect with friends</div> 
+        <GoogleLogin 
               clientId={process.env.REACT_APP_APP_ID}
               buttonText="Log in with Google"
               onSuccess={handleLogin}
@@ -18,7 +29,7 @@ function Signup() {
               cookiePolicy={'single_host_origin'}
         />
 
-        <GoogleLogout/>
+        {/* <GoogleLogout/> */}
     </div>
     );
 }
