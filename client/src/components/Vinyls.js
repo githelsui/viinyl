294134fb39f3880ba3a1683121f7b440458c1681
githelsui <githelsuico@gmail.com>
2022-08-03 04:03:27 -0700

    Revert back here after debugging lol

--- client/src/components/Vinyls.js
+++ client/src/components/Vinyls.js
@@ -7,30 +7,21 @@ import './Vinyls.css'
 /*
  Table display for array of Vinyl component
 */
-const vinyls = [
-    { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
-    { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
-    { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
-    { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
-    { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
-    { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
-    { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
-    { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
-    { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
-    { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
-    { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
-    { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
-];
 
 function Vinyls({albums}) {
     return (
         <div className='vinyl-grid'>
             <Grid container justify='center' spacing={10}>
-                {vinyls.map((vinyl) => (
+                {/* {albums.map((vinyl) => (
                     <Grid item key={vinyl.id}>
                         <Vinyl item={vinyl}/>
                     </Grid>
-                ))}
+                ))} */}
+            {albums.map(function(vinyl, i){
+                return <Grid item key={i}>
+                <Vinyl item={vinyl}/>
+            </Grid>
+            })}
             </Grid>
         </div>
     )
--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -3,25 +3,16 @@ import './Explore.css'
 import Vinyls from '../components/Vinyls';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef} from 'react';
 import usePromise from "react-promise";
 
 const Explore = () => {
-    const [recentReleases, setRecentReleases] = useState([])
+    const recentReleases = useRef(null)
 
     useEffect(() => {
        fetchDiscogDB();
+       console.log(recentReleases)
       }, []);
-
-    //   var Discogs = require('disconnect').Client;
-    //   var dis = new Discogs({userToken: process.env.REACT_APP_DISCOGS_TOKEN});
-    //   var db = dis.database();
-    //   var currentYear = new Date().getFullYear();
-    //   // return await db.search({year: currentYear, country: 'US'})['results'];
-    //   const {recentReleases, loading} =  usePromise(db.search({year: currentYear, country: 'US'}).then(result => result.data));
-    //   if(loading){
-    //     return null;
-    //   }
 
     const fetchDiscogDB = async () => {
         var Discogs = require('disconnect').Client;
@@ -29,14 +20,49 @@ const Explore = () => {
         var db = dis.database();
         var currentYear = new Date().getFullYear();
         // return await db.search({year: currentYear, country: 'US'})['results'];
-        const releases =  await db.search({year: currentYear, country: 'US'}).then(result => result.data);
+        // const releases =  await db.search({year: currentYear, country: 'US'}).then(result => result.data);
         // var recent_releases = value['results'].slice(0, 10)
         // setRecentReleases(value);
         // console.log(recentReleases);
-        setRecentReleases(releases);
-        console.log(recentReleases)
+        var top_releases = []
+        db.search({year: currentYear, country: 'US'}, function(err, data){
+            var recent_releases = data['results'].slice(0, 12)
+            for (var item in recent_releases) {
+                var release = recent_releases[item]
+                var str_chunks = release['title'].split(' - ')
+                var artistStr = str_chunks[0]
+                var albumStr = ''
+                for(var i = 1; i < str_chunks.length; i++){
+                    albumStr += str_chunks[i]
+                }
+                var release_dict = {
+                    id: release['id'],
+                    album: albumStr,
+                    artist: artistStr,
+                    image: release['cover_image'],
+                }
+                top_releases.push(release_dict)
+            }
+            recentReleases.current = top_releases
+            // console.log({recentReleases});
+        });
+        // console.log(recentReleases)
     };
-    
+
+    const vinyls = [
+        { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
+        { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
+        { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
+        { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
+        { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
+        { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
+        { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
+        { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
+        { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
+        { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
+        { id: 1, album: 'Channel Orange', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'}, 
+        { id: 2, album: 'Blonde', artist: 'Frank Ocean', image: 'https://upload.wikimedia.org/wikipedia/en/2/28/Channel_ORANGE.jpg'},
+    ];
 
     return (
         <div className="profile-wrapper">
@@ -46,7 +72,7 @@ const Explore = () => {
                         <div className="subtitle">most recent releases</div>
                 </div>
             </div>
-            <Vinyls albums={recentReleases}/>
+            <Vinyls albums={vinyls}/>
         </div>
     )
 }
