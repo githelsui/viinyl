63932842e442c403c66650842dca211dd57bfdf0
githelsui <githelsuico@gmail.com>
2022-08-05 04:22:59 -0700

    dont change query input in search

--- client/src/components/ExploreComponents/ExploreResultsComponent.js
+++ client/src/components/ExploreComponents/ExploreResultsComponent.js
@@ -5,39 +5,6 @@ import Vinyl from '../Vinyl';
 import { useState, useEffect, useRef} from 'react';
 
 const ExploreResultsComponent = ({query}) => {
-    const [queryResults, setQueryResults] = useState([{}])
-    
-    const fetchQueryResults = async () => {
-        var Discogs = require('disconnect').Client;
-        var dis = new Discogs({userToken: process.env.REACT_APP_DISCOGS_TOKEN});
-        var db = dis.database();
-        var query_Results = []
-        db.search({q: `/${query}`, country: 'US', page: 1, per_page: 30}, function(err, data){
-            var query_data = data['results']
-            for (var item in query_data) {
-                var release = query_data[item]
-                var str_chunks = release['title'].split(' - ')
-                var artistStr = str_chunks[0]
-                var albumStr = ''
-                for(var i = 1; i < str_chunks.length; i++){
-                    albumStr += str_chunks[i]
-                }
-                var release_dict = {
-                    id: release['id'],
-                    album: albumStr,
-                    artist: artistStr,
-                    image: release['cover_image'],
-                }
-                query_Results.push(release_dict)
-            }
-            setQueryResults(query_Results)
-            console.log(query_Results)
-        });
-    };
-
-    useEffect(() => {
-        fetchQueryResults();
-    }, []);
 
     return (
         <div>
@@ -48,7 +15,7 @@ const ExploreResultsComponent = ({query}) => {
                 </div>
             </div>
         </div>
-        <div className='vinyl-grid'>
+        {/* <div className='vinyl-grid'>
             <Grid container justify='center' spacing={10}>
             {queryResults.map(function(vinyl, i){
                 return <Grid item key={i}>
@@ -56,7 +23,7 @@ const ExploreResultsComponent = ({query}) => {
             </Grid>
             })}
             </Grid>
-        </div>
+        </div> */}
     </div>
     );
 };
