8751bff983879c1c8f19a6c263292b962fcb79fb
githelsui <githelsuico@gmail.com>
2022-08-23 19:55:38 -0700

    set up users results tab

--- client/src/components/ExploreComponents/UsersResultsTab.js
+++ client/src/components/ExploreComponents/UsersResultsTab.js
@@ -3,10 +3,26 @@ import './ExploreResults.css'
 import { Grid } from '@material-ui/core';
 import Vinyl from '../Vinyl';
 import { useState, useEffect, useRef} from 'react';
+import { db } from '../util/firebase';
+import { uid } from 'uid';
+import {ref, set, onValue, remove } from "firebase/database";
 
 const UsersResultsTab = ({query}) =>  {
-    //useEffect
-    //keep fetching results from firebase db
+    const [usersList, setUsersList] = useState([])
+
+    const fetchQueryUsers = async () => {
+        //TODO: query unique username
+        onValue(ref(db, 'user/'), snapshot => {
+            const data = snapshot.val();
+            if(data){
+                setUsersList(data);
+            } 
+        });
+    };
+
+    useEffect(() => {
+        fetchQueryUsers();
+     }, []);
     return (
         <div>
         </div>
